3bc144c10ff68424a51b15e430ac043ab130b023
Genne Liu <genneliu@gmail.com>
2021-11-09 16:03:18 -0800

    youtube api

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyAzW0Zn-yiemMUfaoqHlDTRHKR5cuMrjrM";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
