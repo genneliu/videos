134a21b2d373d1eebcc5189f0fc30e284dabe6fa
Genne Liu <genneliu@gmail.com>
2021-11-10 09:34:13 -0800

    video thumbnails

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
   state = { videos: [] };
@@ -17,6 +18,7 @@ class App extends React.Component {
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
         <h4> I have found {this.state.videos.length} videos.</h4>
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
