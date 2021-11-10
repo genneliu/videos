531b6e7af5671f7289c3850acb24d713a505a7b5
Genne Liu <genneliu@gmail.com>
2021-11-10 09:08:01 -0800

    axios

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,20 @@ import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    youtube.get("/search", {
+  state = { videos: [] };
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
+    this.setState({ videos: response.data.items });
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <h4> I have found {this.state.videos.length} videos.</h4>
       </div>
     );
   }
